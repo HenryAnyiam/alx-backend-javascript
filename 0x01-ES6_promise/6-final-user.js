fd1b039c3652b3af3aa0d78fa91d0c64c0345beb
HenryAnyiam <anyiamhenry7@gmail.com>
2023-11-30 00:51:34 +0100

    using Promise.allSettled()

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const file = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, file])
+  .then((values) => values);
+}
