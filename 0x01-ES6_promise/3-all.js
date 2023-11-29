c10f137f547877c2488f58c9c3ba6d1f7c8b7c17
HenryAnyiam <anyiamhenry7@gmail.com>
2023-11-30 00:25:45 +0100

    return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,10 @@ import { uploadPhoto, createUser } from './utils'
 export default function handleProfileSignup() {
   let myPromises = Promise.all([uploadPhoto(), createUser()]);
   myPromises.then(response => {
-    console.log(response[0].body, response[1].firstName, response[1].lastName)
+    const body = response[0].body;
+    const firstName = response[1].firstName;
+    const lastName = response[1].lastName
+    console.log(`${body} ${firstName} ${lastName}`)
   }).catch(error => {console.log("Signup system offline")});
+  return myPromises;
 }
