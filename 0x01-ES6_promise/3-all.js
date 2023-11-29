95a45f18dd485494da0d2b6c95f51666cd966fb2
HenryAnyiam <anyiamhenry7@gmail.com>
2023-11-30 00:11:25 +0100

    using promise.all()

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { uploadPhoto, createUser } from './utils.js'
+
+export default function handleProfileSignup() {
+  let myPromises = Promise.all([uploadPhoto(), createUser()]);
+  myPromises.then(response => {
+    console.log(response[0].body, response[1].firstName, response[1].lastName)
+  }).catch(error => {console.log("Signup system offline")});
+}
