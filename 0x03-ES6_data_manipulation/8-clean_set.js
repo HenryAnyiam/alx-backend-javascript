360fe7f01f2c348d553f92576d538bf6b04da2e9
HenryAnyiam <anyiamhenry7@gmail.com>
2023-12-04 23:46:17 +0100

    set and string methods

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default function cleanSet(set, startString) {
-  if((set instanceof Set) && (startString) && (typeof startString === 'string')) {
+  if((set instanceof Set) && (typeof startString === 'string')) {
     const length = startString.length;
     let final = [];
     if (length == 0) {
