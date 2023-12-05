d35fa3ed1f406f3b233fe836d040d4a88328731e
HenryAnyiam <anyiamhenry7@gmail.com>
2023-12-05 20:52:12 +0100

    WeakMap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!(weakMap.has(endpoint))) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint) + 1;
+    if (count >= 5) {
+      throw new Error("Endpoint load is high");
+    } else {
+      weakMap.set(endpoint, count);
+    }
+  }
+}
+
+export {weakMap, queryAPI};
