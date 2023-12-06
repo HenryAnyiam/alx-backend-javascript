570321da419bc66d11afa82fc2821322a13b0db4
HenryAnyiam <anyiamhenry7@gmail.com>
2023-12-06 21:31:05 +0100

    typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,39 @@
+/* eslint-disable */
+
+interface Teacher {
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
+    [key: string]: any
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => `${firstName.charAt(0)}. ${lastName}`;
+
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
