59390a30c1b62b28885352b9eaad67195392e353
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-30 12:30:32 +0100

    reading from stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.setEncoding('utf8');
 
-console.log("Welcome to Holberton School, what is your name?");
 
 process.stdin.on('data', (data) => {
-  console.log("Your name is: ", data.trim());
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
-  console.log("This important software is now closing");
+  process.stdout.write('This important software is now closing\n');
 });
