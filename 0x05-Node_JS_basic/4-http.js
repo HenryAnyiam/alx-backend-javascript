d1e2b7ba67d831f8adb56f372461abe5f8f38943
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-31 01:00:34 +0100

    http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,15 @@
 /* eslint-disable */
 const http = require("http");
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type' : 'application/json',
     });
     res.end("Hello Holberton School!");
 });
 
-server.listen(1245, () => {
+app.listen(1245, () => {
     console.log("Server is listening on port 1245");
 });
+
+module.exports = app;
